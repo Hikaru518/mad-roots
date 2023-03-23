ad94a242307890f6dd5985bad389542d9e38343a
Hikaru518 <wangpeiguang15@foxmail.com>
2023-03-24 00:38:58 +0800

    结构性重构程序
    - 把（几乎）所有的 config 改到了 config 中
    - 改了一些 typo
    - 根据 PEP8 进行了 format

--- src/CardGen.py
+++ src/CardGen.py
@@ -1,11 +1,80 @@
 from PIL import Image, ImageDraw, ImageFont
+from PIL.ImageFont import FreeTypeFont
 import textwrap
 import csv
 import os
 from shutil import copyfile
+import json
+from src.tools import read_json
+from pathlib import Path
+from pprint import pprint
 
-stdfontsize = 30
-font = ImageFont.truetype("../data/Chusung.ttf", stdfontsize)
+############################################
+# 在这里改参数！
+# TTS_DE_MODE:
+# True: 按照TTS Deck Builder的格式生成文件 每张卡只生成一张图片 例如有3张 105卡牌，则生成文件名是"03x card_105.png"
+#       TTS_DE_MODE 会在OUTPUT_DIR目录下自动生成三个文件夹：initialDeck, market和onBoard，分别对应了玩家初始套牌，市场牌和常驻牌（如向日葵和圣甲虫）
+#       这样的好处是导入进TTS之后会自动按照功能一摞一摞分好，不用再进行整理了。
+# False: 每一张卡牌生成一张图片，有3张105卡牌就会生成3个一样的文件 名为 "card_105_X.png"
+
+# INPUT_CSV_FILE: 一个csv文件，记录了所有卡牌信息，可以直接从V0.6规则中的“导出视图”复制粘贴过来
+# OUTPUT_DIR: 输出路径，最好是一个空文件夹
+# INITIAL_DECK_SETTING: 一个txt文件，记录了玩家初始套牌有哪些卡，格式如："101:6,102:4,103:2"
+# CARDBACK_DIR: 牌背png图像
+
+
+# 注意：
+# 卡牌插图会固定读取路径为"Images/CardIcon/XXX.png"的图片，其中XXX为卡牌数字ID。
+# 卡牌底板在"Images/CardTemplate"文件夹中，会读取卡牌的派系名称，并在此文件夹中找到对应图片作为底板，可以替换这些图片文件但不要改名。
+
+
+# (125,139,152)
+standardWhiteColor = (251, 240, 217)
+standardBlackColor = (65, 65, 65)
+
+
+class Config(object):
+    def __init__(self, config_file_path: str) -> None:
+        config_data = read_json(config_file_path)
+
+        # TTS MODE
+        self.tts_mode = config_data["TTS_DE_MODE"]
+
+        # input and config
+        self.data_dir = Path(config_data["data_dir"]).absolute()
+        self.input_csv_filename = config_data["input_csv_filename"]
+        self.input_csv_filepath = str(self.data_dir / self.input_csv_filename)
+        self.initial_deck_setting = str(
+            self.data_dir / config_data["initial_deck_setting"]
+        )
+        self.cardback_file_path = str(self.data_dir / config_data["cardback_file_name"])
+
+        self.ttf = config_data["ttf"]
+
+        # output
+        self.version = config_data["version"]
+        self.output_dir = Path(config_data["output_dir"]).absolute()
+        self.output_dir_with_version = str(self.output_dir / self.version)
+
+        # font setting
+        self.stdfontsize = 30
+        font_path = Path(self.data_dir / self.ttf).absolute()
+        self.font = ImageFont.truetype(str(font_path), self.stdfontsize)
+
+    @classmethod
+    def get_default_config_path(cls):
+        current_file_path = Path(os.path.abspath(__file__))
+        config_path = current_file_path.parent.parent / "config/config.json"
+        return str(config_path)
+
+    def to_dict(self):
+        return {
+            "TTS_DE_MODE": self.tts_mode,
+            "INPUT_CSV_FILE": self.input_csv_filepath,
+            "INITIAL_DECK_SETTING": self.initial_deck_setting,
+            "OUTPUT_DIR": self.output_dir_with_version,
+            "CARDBACK_DIR": self.cardback_file_path,
+        }
 
 
 class ImageInfo:
@@ -19,14 +88,10 @@ class ImageInfo:
         self.cardID = cardID
 
 
-# (125,139,152)
-standardWhiteColor = (251, 240, 217)
-standardBlackColor = (65, 65, 65)
+def GenImage(info: ImageInfo, outputDir: str, config: Config):
+    font = config.font
 
-
-def GenImage(info: ImageInfo, outputDir: str):
     def DrawText(text, fontsize, xy, colorRGB, anchor):
-        font = ImageFont.truetype("../data/Chusung.ttf", fontsize)
         draw.text(xy, text, fill=colorRGB, font=font, anchor=anchor)
 
     def DrawMultilineText(text, fontsize, xy, colorRGB, anchor):
@@ -69,7 +134,8 @@ def GenImage(info: ImageInfo, outputDir: str):
         classTextColor = (90, 90, 90)
         descriptionColor = (128, 139, 151)
         cardNameColor = standardWhiteColor
-    templateFullDir = os.path.join("../data/CardTemplate", templateDir)
+    card_template = Path(config.data_dir) / "CardTemplate"
+    templateFullDir = str(card_template / templateDir)
 
     img = Image.open(templateFullDir)
     draw = ImageDraw.Draw(img)
@@ -89,7 +155,7 @@ def GenImage(info: ImageInfo, outputDir: str):
     # discription
     DrawMultilineText(info.description, 48, (100, 864), descriptionColor, "lm")
 
-    fullIconDir = "../data/CardIcon/{0}.png".format(info.cardID)
+    fullIconDir = str(config.data_dir / f"CardIcon/{info.cardID}.png")
     if os.path.exists(fullIconDir):
         cardIcon = Image.open(fullIconDir)
         cmx, cmy = cardIcon.size
@@ -104,104 +170,100 @@ def GenImage(info: ImageInfo, outputDir: str):
     img.save(outputDir)
 
 
-############################################
-# 在这里改参数！
-# TTS_DE_MODE:
-# True: 按照TTS Deck Builder的格式生成文件 每张卡只生成一张图片 例如有3张 105卡牌，则生成文件名是"03x card_105.png"
-#       TTS_DE_MODE 会在OUTPUT_DIR目录下自动生成三个文件夹：initialDeck, market和onBoard，分别对应了玩家初始套牌，市场牌和常驻牌（如向日葵和圣甲虫）
-#       这样的好处是导入进TTS之后会自动按照功能一摞一摞分好，不用再进行整理了。
-# False: 每一张卡牌生成一张图片，有3张105卡牌就会生成3个一样的文件 名为 "card_105_X.png"
+def convert_to_images(config_file: str):
+    config = Config(config_file)
+    pprint(config.to_dict())
 
-# INPUT_DIR: 一个csv文件，记录了所有卡牌信息，可以直接从V0.6规则中的“导出视图”复制粘贴过来
-# OUTPUT_DIR: 输出路径，最好是一个空文件夹
-# INITIAL_DECK_SETTING: 一个txt文件，记录了玩家初始套牌有哪些卡，格式如："101:6,102:4,103:2"
-# CARDBACK_DIR: 牌背png图像
+    # 参数
+    TTS_DE_MODE = config.tts_mode
+    INPUT_CSV_FILE = config.input_csv_filepath  # input csv file
+    INITIAL_DECK_SETTING = config.initial_deck_setting
+    OUTPUT_DIR = config.output_dir_with_version
+    CARDBACK_DIR = config.cardback_file_path
 
+    initialDeckDict = dict()
+    with open(INITIAL_DECK_SETTING, newline="", encoding="utf-8") as initialSettingFile:
+        initialString = initialSettingFile.readline()
+        cardList = initialString.split(",")
+        for pairString in cardList:
+            pair = pairString.split(":")
+            initialCardId = int(pair[0])
+            initialCardNumber = int(pair[1])
+            initialDeckDict[initialCardId] = initialCardNumber
+    print("初始套牌：" + str(initialDeckDict))
 
-# 注意：
-# 卡牌插图会固定读取路径为"Images/CardIcon/XXX.png"的图片，其中XXX为卡牌数字ID。
-# 卡牌底板在"Images/CardTemplate"文件夹中，会读取卡牌的派系名称，并在此文件夹中找到对应图片作为底板，可以替换这些图片文件但不要改名。
+    with open(INPUT_CSV_FILE, newline="", encoding="utf-8") as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # 跳过第一行，即标题行
+        completed = 0
 
-# 参数
-TTS_DE_MODE = True
-INPUT_DIR = "../data/cardInfo.csv"
-INITIAL_DECK_SETTING = "../data/initialDeck.txt"
-OUTPUT_DIR = "../output/v0.603/"
-CARDBACK_DIR = "../data/back.png"
+        if not os.path.exists(OUTPUT_DIR):
+            Path(OUTPUT_DIR).mkdir()
+        if not os.path.exists(os.path.join(OUTPUT_DIR, "market")):
+            os.mkdir(os.path.join(OUTPUT_DIR, "market"))
+        if not os.path.exists(os.path.join(OUTPUT_DIR, "onBoard")):
+            os.mkdir(os.path.join(OUTPUT_DIR, "onBoard"))
+        if not os.path.exists(os.path.join(OUTPUT_DIR, "initialDeck")):
+            os.mkdir(os.path.join(OUTPUT_DIR, "initialDeck"))
 
-initialDeckDict = dict()
-with open(INITIAL_DECK_SETTING, newline="", encoding="utf-8") as initialSettingFile:
-    initialString = initialSettingFile.readline()
-    cardList = initialString.split(",")
-    for pairString in cardList:
-        pair = pairString.split(":")
-        initialCardId = int(pair[0])
-        initialCardNumber = int(pair[1])
-        initialDeckDict[initialCardId] = initialCardNumber
-print("初始套牌：" + str(initialDeckDict))
+        for row in reader:
+            # 处理每一行数据 type, classText, sunCost, coinCost, name, desciption, cardID
+            print("{0} {1}".format(completed, row))
+            type = row[2]
+            classText = row[2] + " - " + row[1]
+            sunCost = row[4]
+            coinCost = row[3]
+            name = row[0]
+            description = row[5]
+            repeatCount = int(row[6])
+            cardID = int(row[7])
+            imageInfo = ImageInfo(
+                type, classText, sunCost, coinCost, name, description, cardID
+            )
+            if TTS_DE_MODE:
+                if row[2] == "常驻":
+                    outputDir = os.path.join(
+                        OUTPUT_DIR,
+                        "onBoard",
+                        "{0:0>2}x card_{1}.png".format(repeatCount, cardID),
+                    )
+                    GenImage(imageInfo, outputDir, config)
+                else:
+                    outputDir = os.path.join(
+                        OUTPUT_DIR,
+                        "market",
+                        "{0:0>2}x card_{1}.png".format(repeatCount, cardID),
+                    )
+                    GenImage(imageInfo, outputDir, config)
 
+                # 生成初始卡组
+                if cardID in initialDeckDict.keys():
+                    outputDir = os.path.join(
+                        OUTPUT_DIR,
+                        "initialDeck",
+                        "{0:0>2}x card_{1}.png".format(initialDeckDict[cardID], cardID),
+                    )
+                    GenImage(imageInfo, outputDir, config)
 
-with open(INPUT_DIR, newline="", encoding="utf-8") as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader)  # 跳过第一行，即标题行
-    completed = 0
-
-    if not os.path.exists(os.path.join(OUTPUT_DIR, "market")):
-        os.mkdir(os.path.join(OUTPUT_DIR, "market"))
-    if not os.path.exists(os.path.join(OUTPUT_DIR, "onBoard")):
-        os.mkdir(os.path.join(OUTPUT_DIR, "onBoard"))
-    if not os.path.exists(os.path.join(OUTPUT_DIR, "initialDeck")):
-        os.mkdir(os.path.join(OUTPUT_DIR, "initialDeck"))
-
-    for row in reader:
-        # 处理每一行数据 type, classText, sunCost, coinCost, name, desciption, cardID
-        print("{0} {1}".format(completed, row))
-        type = row[2]
-        classText = row[2] + " - " + row[1]
-        sunCost = row[4]
-        coinCost = row[3]
-        name = row[0]
-        description = row[5]
-        repeatCount = int(row[6])
-        cardID = int(row[7])
-        imageInfo = ImageInfo(
-            type, classText, sunCost, coinCost, name, description, cardID
-        )
-        if TTS_DE_MODE:
-            if row[2] == "常驻":
-                outputDir = os.path.join(
-                    OUTPUT_DIR,
-                    "onBoard",
-                    "{0:0>2}x card_{1}.png".format(repeatCount, cardID),
-                )
-                GenImage(imageInfo, outputDir)
             else:
-                outputDir = os.path.join(
-                    OUTPUT_DIR,
-                    "market",
-                    "{0:0>2}x card_{1}.png".format(repeatCount, cardID),
-                )
-                GenImage(imageInfo, outputDir)
+                for i in range(repeatCount):
+                    outputDir = os.path.join(
+                        OUTPUT_DIR, "card_{0}_{1}.png".format(cardID, i + 1)
+                    )
+                    GenImage(imageInfo, outputDir, config)
+            completed += 1
 
-            # 生成初始卡组
-            if cardID in initialDeckDict.keys():
-                outputDir = os.path.join(
-                    OUTPUT_DIR,
-                    "initialDeck",
-                    "{0:0>2}x card_{1}.png".format(initialDeckDict[cardID], cardID),
-                )
-                GenImage(imageInfo, outputDir)
+        # 卡背 png 图像
+        if TTS_DE_MODE:
+            copyfile(CARDBACK_DIR, os.path.join(OUTPUT_DIR, "market", "00 Back.png"))
+            copyfile(CARDBACK_DIR, os.path.join(OUTPUT_DIR, "onBoard", "00 Back.png"))
+            copyfile(
+                CARDBACK_DIR, os.path.join(OUTPUT_DIR, "initialDeck", "00 Back.png")
+            )
 
-        else:
-            for i in range(repeatCount):
-                outputDir = os.path.join(
-                    OUTPUT_DIR, "card_{0}_{1}.png".format(cardID, i + 1)
-                )
-                GenImage(imageInfo, outputDir)
-        completed += 1
 
-    # 卡背png图像
-    if TTS_DE_MODE:
-        copyfile(CARDBACK_DIR, os.path.join(OUTPUT_DIR, "market", "00 Back.png"))
-        copyfile(CARDBACK_DIR, os.path.join(OUTPUT_DIR, "onBoard", "00 Back.png"))
-        copyfile(CARDBACK_DIR, os.path.join(OUTPUT_DIR, "initialDeck", "00 Back.png"))
+if __name__ == "__main__":
+    # default config path
+    default_config_path = Config.get_default_config_path()
+
+    convert_to_images(default_config_path)
