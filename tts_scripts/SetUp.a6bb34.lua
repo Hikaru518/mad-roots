50b902b88802e730544147b97e1f43052bf8c73c
Hikaru518 <wangpeiguang15@foxmail.com>
2023-07-01 21:12:01 +0800

    加入根系卡的清理脚本

--- tts_scripts/SetUp.a6bb34.lua
+++ tts_scripts/SetUp.a6bb34.lua
@@ -1,5 +1,12 @@
 MARKET_DECK = Global.getVar('MARKET_DECK')
+
 SEED_DECK = Global.getVar('SEED_DECK')
+SEED_START_COOD_0 = Global.getVar('SEED_START_COOD_0')
+SEED_START_COOD_1 = Global.getVar('SEED_START_COOD_1')
+SEED_START_COOD_2 = Global.getVar('SEED_START_COOD_2')
+SEED_DIFF = Global.getVar('SEED_DIFF')
+
+MARKET_CARD_DIFF = Global.getVar('MARKET_CARD_DIFF')
 
 ROOT_MAP_START_COOD_xPos = Global.getVar('ROOT_MAP_START_COOD_0')
 ROOT_MAP_START_COOD_yPos = Global.getVar('ROOT_MAP_START_COOD_2')
@@ -9,6 +16,8 @@ ROOT_MAP_DIFF = Global.getVar('ROOT_MAP_DIFF')
 GOOD_SOIL_DECK = Global.getVar('GOOD_SOIL_DECK')
 BAD_SOIL_DECK = Global.getVar('BAD_SOIL_DECK')
 STONE_BAG = Global.getVar('STONE_BAG')
+
+
 
 math.randomseed(os.time())
 
@@ -35,37 +44,36 @@ function shuffle(array)
     return output
 end
 
-
 function setUpMarket()
     local deck = getObjectFromGUID(MARKET_DECK)
     deck.shuffle()
 
     local deckPos = deck.getPosition()
-    local xPos = deckPos[1] + 5.3
-    for i = 1, 7 do
+    local xPos = deckPos[1] + MARKET_CARD_DIFF
+    for i = 1, 6 do
         deck.takeObject({ flip = true, position = { xPos, deckPos[2], deckPos[3] } })
-        xPos = xPos + 5.3
+        xPos = xPos + MARKET_CARD_DIFF
     end
 end
-
 
 function setUpSeed()
     local deck = getObjectFromGUID(SEED_DECK)
     deck.shuffle()
 
-    local deckPos = deck.getPosition()
-    local posDiff = 2.45
-    local xPos = deckPos[1] + posDiff
+    local posDiff = SEED_DIFF
+    local xPos = SEED_START_COOD_0
+    local yPos = SEED_START_COOD_1
+    local zPos = SEED_START_COOD_2
+
     for i = 1, 3 do
-        local yPos = deckPos[3]
+        xPos = SEED_START_COOD_0
         for j = 0, 2 do
-            deck.takeObject({ flip = true, position = { xPos, deckPos[2], yPos } })
-            yPos = yPos + posDiff
+            deck.takeObject({ flip = true, position = { xPos, yPos, zPos } })
+            xPos = xPos + posDiff
         end
-        xPos = xPos + posDiff
+        zPos = zPos - posDiff
     end
 end
-
 
 function getCartesianProduct(x_list, y_list)
     local result = {}
@@ -78,7 +86,6 @@ function getCartesianProduct(x_list, y_list)
     return result
 end
 
-
 function initMapSetting()
     local good_soil_deck = getObjectFromGUID(GOOD_SOIL_DECK)
     local bad_soil_deck = getObjectFromGUID(BAD_SOIL_DECK)
@@ -90,13 +97,15 @@ function initMapSetting()
 
     cood_list = shuffle(cood_list)
 
+    -- place good soil
     for i = 1, 3 do
         local cood = cood_list[i]
         local xPos = ROOT_MAP_START_COOD_xPos + ROOT_MAP_DIFF * (cood[1] - 1)
         local yPos = ROOT_MAP_START_COOD_yPos - ROOT_MAP_DIFF * (cood[2] - 1)
-        good_soil_deck.takeObject({ flip = false, position = { xPos, ROOT_MAP_START_COOD_zPos + 2, yPos } })
+        good_soil_deck.takeObject({ flip = true, position = { xPos, ROOT_MAP_START_COOD_zPos + 2, yPos } })
     end
 
+    -- place bad soil
     for i = 4, 6 do
         local cood = cood_list[i]
         local xPos = ROOT_MAP_START_COOD_xPos + ROOT_MAP_DIFF * (cood[1] - 1)
@@ -104,14 +113,14 @@ function initMapSetting()
         bad_soil_deck.takeObject({ flip = false, position = { xPos, ROOT_MAP_START_COOD_zPos + 2, yPos } })
     end
 
-    for i = 7, 7 do
+    -- place stonef
+    for i = 7, 8 do
         local cood = cood_list[i]
         local xPos = ROOT_MAP_START_COOD_xPos + ROOT_MAP_DIFF * (cood[1] - 1)
         local yPos = ROOT_MAP_START_COOD_yPos - ROOT_MAP_DIFF * (cood[2] - 1)
         stone_bag.takeObject({ flip = false, position = { xPos, ROOT_MAP_START_COOD_zPos + 2, yPos } })
     end
 end
-
 
 function setUp()
     setUpMarket()
