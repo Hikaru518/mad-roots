aaded675094ab068be768548f86c765d7573b6db
playground <wangpeiguangwpg@gmail.com>
2023-07-09 10:57:39 +0800

    改善 watermark 脚本 (#12)
    * 改善 watermark 脚本
    
    1. 配置和文件分离，配置文件中含有敏感信息，现在分离至 watermark.json 文件中。
    2. 脚本现在可以支持直接输入 input directory 和 output directory，不必再和 script 在同一文件夹下。
    3. 可读性改善。
    4. reformatted by black
    
    * update readme

--- watermark/watermark.py
+++ watermark/watermark.py
@@ -1,11 +1,20 @@
-from blind_watermark import WaterMark
+import blind_watermark
 import argparse
 import os
+import json
+from typing import Dict
+from pathlib import Path
 
 
-def get_alldirectories(folder):
+def read_json(file_path: str) -> Dict:
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    return data
+
+
+def get_alldirectories(folder: str):
     subdirectories = []
-    for root, dirs, files in os.walk(folder):
+    for root, dirs, _ in os.walk(folder):
         for dir in dirs:
             subdirectories.append(os.path.join(root, dir))
     subdirectories.append(folder)
@@ -16,49 +25,66 @@ def get_alldirectories(folder):
     return all_directories
 
 
-def add_watermark(file: str, input: str, output: str) -> None:
-    if not file.endswith(".jpg") and not file.endswith(".png"):
-        return
-    input_file = os.path.join(input, file)
-    if not os.path.exists(input_file):
-        print("add file not found", input_file)
-        return
-    if not os.path.exists(output):
-        os.makedirs(output)
-    print("adding watermark to " + input_file)
-    # read the picture
-    bwm = WaterMark(password_img=2023, password_wm=2023)
-    bwm.read_img(input_file)
-    # read the watermark
-    watermark = "PEANUX_VAPOURBOX_2023_DICECON"
-    bwm.read_wm(watermark, mode="str")
-    # embed the watermark
-    output_img = os.path.join(output, file)
-    bwm.embed(output_img)
-    len_wm = len(bwm.wm_bit)
-    # check if len_wm.txt exists
-    if not os.path.exists(input + "/len_wm.txt"):
-        # make a new file
-        with open(input + "/len_wm.txt", "w") as f:
-            f.write(file + " " + str(len_wm) + "\n")
-    else:
-        with open(input + "/len_wm.txt", "a") as f:
-            f.write(file + " " + str(len_wm) + "\n")
-    print("put down the length of wm_bit {len_wm}".format(len_wm=len_wm))
-    print("watermark added to " + file)
+class WaterMark(object):
+    def __init__(self, config_file_path: str = "") -> None:
+        if config_file_path == "":
+            defaul_config_path = os.path.join(
+                os.path.dirname(__file__), "watermark.json"
+            )
+            config_data = read_json(defaul_config_path)
+        else:
+            config_data = read_json(config_file_path)
 
+        # input and config
+        self._water_mark_text = config_data["water_mark_text"]
+        self._password_img = config_data["password_img"]
+        self._password_wm = config_data["password_wm"]
 
-def extract_watermark(file: str, watermark_length: int) -> None:
-    if not file.endswith(".jpg") and not file.endswith(".png"):
-        return
-    if not os.path.exists(file):
-        print("extract file not found", file)
-        return
-    print("extracting watermark from " + file)
-    # read the picture
-    bwm = WaterMark(password_img=2023, password_wm=2023)
-    wm_extract = bwm.extract(file, wm_shape=watermark_length, mode="str")
-    print("watermark is ", wm_extract)
+        self.bwm = blind_watermark.WaterMark(
+            password_img=self._password_img, password_wm=self._password_wm
+        )
+
+    def add_watermark(self, file: str, input: str, output: str) -> None:
+        input_file = os.path.join(input, file)
+        if not os.path.exists(input_file):
+            raise Exception(f"input file not found, input path is: {input_file}")
+
+        if not file.endswith(".jpg") and not file.endswith(".png"):
+            print(f"skip non image files... current skipped file is: {file}")
+            return
+
+        if not os.path.exists(output):
+            os.makedirs(output)
+
+        print("adding watermark to " + input_file)
+        # read the picture
+        self.bwm.read_img(input_file)
+        # read the watermark
+        watermark = self._water_mark_text
+        self.bwm.read_wm(watermark, mode="str")
+        # embed the watermark
+        output_img = os.path.join(output, file)
+        self.bwm.embed(output_img)
+        len_wm = len(self.bwm.wm_bit)
+
+        with open(output + "/len_wm.txt", "a+") as f:
+            f.write(file + " " + str(len_wm) + "\n")
+
+        print("put down the length of wm_bit {len_wm}".format(len_wm=len_wm))
+        print("watermark added to " + file)
+
+    def extract_watermark(self, file: str, watermark_length: int) -> None:
+        if not os.path.exists(file):
+            raise Exception(f"extract file not found, path is: {file}")
+
+        if not file.endswith(".jpg") and not file.endswith(".png"):
+            print(f"skip non image files... current skipped file is: {file}")
+
+        print("extracting watermark from " + file)
+
+        # read the picture
+        wm_extract = self.bwm.extract(file, wm_shape=watermark_length, mode="str")
+        print("watermark is ", wm_extract)
 
 
 if __name__ == "__main__":
@@ -73,9 +99,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     current_directory = os.getcwd()
+    wm = WaterMark()
     if args.add != "" and args.out != "":
-        input_directory = os.path.join(current_directory, args.add)
-        output_directory = os.path.join(current_directory, args.out)
+        input_directory = str(Path(args.add).absolute())
+        output_directory = str(Path(args.out).absolute())
         all_directories = get_alldirectories(input_directory)
         for directory in all_directories:
             subdirectory = os.path.join(input_directory, directory)
@@ -83,14 +110,14 @@ if __name__ == "__main__":
             for file in os.listdir(subdirectory):
                 if file.endswith("/"):
                     continue
-                add_watermark(
+                wm.add_watermark(
                     file,
                     os.path.join(input_directory, directory),
                     os.path.join(output_directory, directory),
                 )
     elif args.extract != "" and args.watermark_length != 0:
-        extract_file = os.path.join(current_directory, args.extract)
-        extract_watermark(extract_file, args.watermark_length)
+        extract_file = str(Path(args.extract).absolute())
+        wm.extract_watermark(extract_file, args.watermark_length)
     else:
         print(
             "invalid arguments, input add dir + out dir or extract image + watermark length"
